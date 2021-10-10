052fefb1f1932da633e2a2355ee9b30feddac116
Cecilia Chan <75919064+ceciliachan1979@users.noreply.github.com>
2021-10-10 12:47:28 -0700

    Hook into the C# Source Generation APIs (#3)

---
+++ Sources/FileToString/SourceGenerator.cs
@@ -0,0 +1,62 @@
+﻿namespace FileToString
+{
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using Microsoft.CodeAnalysis.Text;
+    using System.IO;
+    using System.Text;
+    using System.Threading;
+
+    [Generator]
+    public class SourceGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            foreach (AdditionalText additionalText in context.AdditionalFiles)
+            {
+                string filename = Path.GetFileName(additionalText.Path);
+                if (Path.GetExtension(filename) == ".string")
+                {
+                    string namespaceName = null;
+                    string visibility = null;
+                    string className = null;
+                    string fieldName = null;
+
+                    AnalyzerConfigOptions options = context.AnalyzerConfigOptions.GetOptions(additionalText);
+
+                    options.TryGetValue("build_metadata.additionalfiles.Namespace", out namespaceName);
+                    options.TryGetValue("build_metadata.additionalfiles.Visibility", out visibility);
+                    options.TryGetValue("build_metadata.additionalfiles.ClassName", out className);
+                    options.TryGetValue("build_metadata.additionalfiles.FieldName", out fieldName);
+
+                    if (string.IsNullOrEmpty(visibility))
+                    {
+                        visibility = "public";
+                    }
+
+                    if (string.IsNullOrEmpty(className))
+                    {
+                        className = "StringLiterals";
+                    }
+
+                    if (string.IsNullOrEmpty(fieldName))
+                    {
+                        fieldName = Path.GetFileNameWithoutExtension(filename);
+                    }
+
+                    SourceText text = additionalText.GetText(CancellationToken.None);
+
+                    if (text != null)
+                    {
+                        string source = new ClassGenerator(namespaceName, visibility, className, fieldName).Generate(text.ToString());
+                        context.AddSource(Path.GetFileNameWithoutExtension(filename) + ".cs", source);
+                    }
+                }
+            }
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+    }
+}
--- Tests/FileToString.UnitTests/ClassGenerator.cs
+++ Tests/FileToString.UnitTests/ClassGenerator.cs
@@ -2,21 +2,14 @@ namespace FileToString.UnitTests
 {
     using Xunit;
 
-    public class ClassGenerator
+    public class ClassGeneratorTests
     {
         [Fact]
         public void TestGeneratePublicSimpleString()
         {
-            FileToString.ClassGenerator sourceGenerator = new FileToString.ClassGenerator("TestNamespace", "public", "TestStrings", "TestField");
-            string result = sourceGenerator.Generate("The test works!");
-            Assert.Equal(@"namespace TestNamespace
-{
-    public static partial class TestStrings
-    {
-        public static string TestField = @""The test works!"";
-    }
-}
-", result);
+            ClassGenerator classGenerator = new ClassGenerator("TestNamespace", "public", "TestStrings", "TestField");
+            string result = classGenerator.Generate("The test works!");
+            Assert.Equal(UnitTestSourceTexts.Case1Test, result);
         }
     }
 }
